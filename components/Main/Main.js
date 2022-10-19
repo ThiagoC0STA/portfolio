97f01fd9fa0ebbc1c80b69477df8809f2ce00d0e
Thiago Rodrigues da Costa <82855801+ThiagoC0STA@users.noreply.github.com>
2022-10-19 12:56:39 -0300

    pre-load image

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -10,11 +10,29 @@ const Main = () => {
       <h1>Thiago Costa</h1>
       <p>Front-End developer</p>
       <div>
-        <a href="https://www.linkedin.com/feed/update/urn:li:activity:6938252803970592768/" target="_blank" id="cv-button">Download CV</a>
-        <a href="https://github.com/thiagoc0sta" target="__blank" id="github-button">My github</a>
+        <a
+          href="https://www.linkedin.com/feed/update/urn:li:activity:6938252803970592768/"
+          target="_blank"
+          id="cv-button"
+        >
+          Download CV
+        </a>
+        <a
+          href="https://github.com/thiagoc0sta"
+          target="__blank"
+          id="github-button"
+        >
+          My github
+        </a>
       </div>
       <AvatarDiv>
-        <Image src={Avatar} alt="Avatar" layout="fill" quality={100} />
+        <Image
+          src={Avatar}
+          alt="Avatar"
+          layout="fill"
+          property={true}
+          quality={100}
+        />
       </AvatarDiv>
     </MainDiv>
   );
