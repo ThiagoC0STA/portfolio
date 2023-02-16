1b5882dbac4c18288d8d96bfdd4dad606926b106
Thiago Rodrigues da Costa <82855801+ThiagoC0STA@users.noreply.github.com>
2023-02-16 20:17:28 -0300

    animation issues

--- components/FadeIn/styled.ts
+++ components/FadeIn/styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const FadeInDiv = styled.div<{ show: boolean }>`
   opacity: 0;
   transform: translateY(5%);
-  transition: all 0.7s ease-in 0.2s;
+  transition: all 0.4s ease-in 0.2s;
 
   ${({ show }) =>
     show &&
