e3386974592e6b5428346ed71a5f53a0106e84e4
ThiagoC0STA <thiago@lacostacorretora.com.br>
2025-05-19 11:45:18 -0300

    clickable card

--- components/recent-projects.tsx
+++ components/recent-projects.tsx
@@ -91,86 +91,88 @@ export const RecentProjects = () => {
               key={id}
               className="flex h-[30rem] w-[90vw] items-center justify-center sm:h-[41rem] sm:w-[570px] lg:min-h-[32.5rem]"
             >
-              <PinContainer title="Visit" href={link}>
-                <div className="relative mb-10 flex h-[18rem] w-[80vw] items-center justify-center overflow-hidden sm:h-[27rem] sm:w-[570px]">
-                  <div className="relative h-full w-full overflow-hidden bg-[#13162d] lg:rounded-3xl">
+              <Link href={link} target="_blank" rel="noreferrer noopener" className="w-full">
+                <PinContainer title="Visit">
+                  <div className="relative mb-10 flex h-[18rem] w-[80vw] items-center justify-center overflow-hidden sm:h-[27rem] sm:w-[570px]">
+                    <div className="relative h-full w-full overflow-hidden bg-[#13162d] lg:rounded-3xl">
+                      <Image
+                        height={330}
+                        width={552}
+                        src="/bg.png"
+                        alt="bg-img"
+                        quality={100}
+                      />
+                    </div>
+
                     <Image
-                      height={330}
-                      width={552}
-                      src="/bg.png"
-                      alt="bg-img"
+                      height={0}
+                      width={1000}
+                      src={img}
+                      alt={title}
                       quality={100}
+                      className="absolute -bottom-24 z-10 -rotate-2 rounded-tr-3xl rounded-tl-3xl w-[500px] h-full object-cover object-top"
                     />
                   </div>
 
-                  <Image
-                    height={0}
-                    width={1000}
-                    src={img}
-                    alt={title}
-                    quality={100}
-                    className="absolute -bottom-24 z-10 -rotate-2 rounded-tr-3xl rounded-tl-3xl w-[500px] h-full object-cover object-top"
-                  />
-                </div>
+                  <h1 className="line-clamp-1 text-base font-bold md:text-xl lg:text-2xl">
+                    {title}
+                  </h1>
 
-                <h1 className="line-clamp-1 text-base font-bold md:text-xl lg:text-2xl">
-                  {title}
-                </h1>
+                  <p className="line-clamp-2 text-sm font-light lg:text-xl lg:font-normal">
+                    {des}
+                  </p>
 
-                <p className="line-clamp-2 text-sm font-light lg:text-xl lg:font-normal">
-                  {des}
-                </p>
-
-                <div className="mb-3 mt-7 flex items-center justify-between">
-                  <div className="flex items-center">
-                    {iconLists.map(({ Icon, color }, i) => (
-                      <span
-                        key={i}
-                        className="flex h-8 w-8 items-center justify-center rounded-full border border-white/[0.2] bg-black lg:h-10 lg:w-10"
-                        style={{
-                          transform: `translateX(-${5 * i * 2}px)`,
-                        }}
-                      >
-                        <Icon className="text-2xl" style={{ color }} />
-                      </span>
-                    ))}
-                  </div>
-
-                  <div className="flex items-center justify-center">
-                    {sourceCode ? (
-                      <>
-                        <Link
-                          href={sourceCode}
-                          target="_blank"
-                          rel="noreferrer noopener"
-                          className="flex text-sm text-purple md:text-xs lg:text-[18px]"
+                  <div className="mb-3 mt-7 flex items-center justify-between">
+                    <div className="flex items-center">
+                      {iconLists.map(({ Icon, color }, i) => (
+                        <span
+                          key={i}
+                          className="flex h-8 w-8 items-center justify-center rounded-full border border-white/[0.2] bg-black lg:h-10 lg:w-10"
+                          style={{
+                            transform: `translateX(-${5 * i * 2}px)`,
+                          }}
                         >
-                          Source Code
-                        </Link>
-                        <FaLocationArrow className="ms-2" color="#cbacf9" />
-                      </>
-                    ) : (
-                      <span className="flex items-center text-sm text-gray-400 md:text-xs lg:text-[18px]">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-4 w-4 mr-1"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
-                          />
-                        </svg>
-                        Private Code
-                      </span>
-                    )}
+                          <Icon className="text-2xl" style={{ color }} />
+                        </span>
+                      ))}
+                    </div>
+
+                    <div className="flex items-center justify-center" onClick={(e) => e.stopPropagation()}>
+                      {sourceCode ? (
+                        <>
+                          <Link
+                            href={sourceCode}
+                            target="_blank"
+                            rel="noreferrer noopener"
+                            className="flex text-sm text-purple md:text-xs lg:text-[18px]"
+                          >
+                            Source Code
+                          </Link>
+                          <FaLocationArrow className="ms-2" color="#cbacf9" />
+                        </>
+                      ) : (
+                        <span className="flex items-center text-sm text-gray-400 md:text-xs lg:text-[18px]">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-4 w-4 mr-1"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                            />
+                          </svg>
+                          Private Code
+                        </span>
+                      )}
+                    </div>
                   </div>
-                </div>
-              </PinContainer>
+                </PinContainer>
+              </Link>
             </div>
           )
         )}
